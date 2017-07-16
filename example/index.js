bd6a37c03de87b26ad87428755b87d916178ac91
ziaochina <ziaochina@gmail.com>
2017-07-16 13:29:19 +0800

    zlj

---
+++ example/index.js
@@ -0,0 +1,21 @@
+import { config, start, componentFactory } from 'mk-meta-engine'
+import myConfig  from './config'
+
+import mk_template_login from './apps/mk-template-login/index.js'
+
+const apps = {
+	[mk_template_login.name]:mk_template_login,	
+}
+
+
+config(myConfig({apps}))
+
+
+import * as mkComponents from 'mk-component'
+
+Object.keys(mkComponents).forEach(key=>{
+	componentFactory.registerComponent(key, mkComponents[key])
+})
+	
+
+start()
